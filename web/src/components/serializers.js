97408b0fa013036c19b001c5285c4031f519f677
Justin <JEStaubach@users.noreply.github.com>
2020-09-06 10:37:43 -0400

    add code blocks syntax highlighter

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Figure from './Figure'
+import SyntaxHighlighter from 'react-syntax-highlighter'
 
 const serializers = {
   types: {
     authorReference: ({node}) => <span>{node.author.name}</span>,
-    mainImage: Figure
+    mainImage: Figure,
+    code: ({node = {}}) => {
+      const { code, language } = node
+      if (!code) return null;
+      return <SyntaxHighlighter language={language || 'text' }>{code}</SyntaxHighlighter>
+    }
   }
 }
 
