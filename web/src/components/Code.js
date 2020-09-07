3d7fc6b7ee3326fe4a7cfecf10f2ea57effa651f
Justin <justin@jsdevtools.com>
2020-09-07 00:28:52 -0400

    add dark mode to code

--- web/src/components/Code.js
+++ web/src/components/Code.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 export default ({node = {}}) => {
   const { code, language } = node
   if (!code) return null;
-  return <SyntaxHighlighter language={language || 'text' }>{code}</SyntaxHighlighter>
+  return <SyntaxHighlighter customStyle={{fontSize: '.8em'}} style={dark} language={language || 'text' }>{code}</SyntaxHighlighter>
 }
